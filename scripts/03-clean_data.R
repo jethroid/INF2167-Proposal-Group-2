243d7bdf120a23305bc6847cb793801be71b042b
William Chan <singhon.chan@mail.utoronto.ca>
2026-08-01 16:19:04 -0400

    Move merging scripts to scripts folder, amend paper structure

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,138 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Clean and Merge datasets
+# Author: William Chan
+# Date: 1 August 2026
 
 #### Workspace setup ####
 library(tidyverse)
+library(here)
+library(sf)
+library(tinytable)
+library(scales)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+#### Spatial Data ####
+# import spatial data with simple feature package
+fed_boundaries <- st_read(
+  here("data", "01-raw_data", "geography", "FED_2021", "FED_2021.shp"))
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
+# rename federal electoral district ID column
+fed_boundaries <- fed_boundaries |>
+  rename(FEDDGUID_CEFIDUGD = DGUID) |>
+  select(FEDDGUID_CEFIDUGD,FEDENAME,PRUID,geometry) # filter columns of interests
+
+### Local News Data ###
+# load local news data
+news <- read_csv((here("data", "01-raw_data", "paper_data",  "local_news.csv")))
+
+# split the coordinates in news data into latitude and longitude
+news <- news |>
+  separate(coordinates,
+           into = c("latitude", "longitude"),
+           sep = ", ",
+           convert = TRUE
+  )
+
+# convert news data into spatial data with the coordinates, and assign 4326 (WGS84) as reference coordinate system
+news_sf <- st_as_sf(news, coords = c("longitude", "latitude"), crs = 4326)
+
+# align the coordinate system of news data with the federal electoral district boundaries data
+news_sf <- st_transform(news_sf, st_crs(fed_boundaries))
+
+# merge the news data with federal electoral district boundaries data
+# if a point (address of local news) is located within a polygon (federal boundaries), it will be assigned the corresponding district ID and name 
+news_fed <- st_join(news_sf, fed_boundaries, join = st_within)
+
+# check unmatched result that has no district ID assigned
+news_fed |>
+  filter(is.na(FEDDGUID_CEFIDUGD)) |>
+  select(title, Community, geometry)
+
+# manually correct the coordinates for the unmatched data
+new_point <- st_sfc(
+  st_point(c(-123.11413726425735, 49.27975314823103)), crs = 4326) |>
+  st_transform(st_crs(news_sf))
+
+news_sf$geometry[
+  news_sf$title == "Radio-Canada TV - Vancouver"
+] <- new_point[[1]]
+
+# ensure coordinate system equals to the federal boundaries file
+news_sf <- st_transform(
+  news_sf,
+  st_crs(fed_boundaries)
+)
+
+# merge again
+news_fed <- st_join(news_sf, fed_boundaries, join = st_within)
+
+
+### Elections data ###
+# load election results
+election_2025 <- read_csv((here("data", "01-raw_data", "paper_data",  "election_2025_ajax.csv")))
+election_2021 <- read_csv((here("data", "01-raw_data", "paper_data",  "election_2021_ajax.csv")))
+election_2019 <- read_csv((here("data", "01-raw_data", "paper_data",  "election_2019_ajax.csv")))
+
+# create a function to clean election data
+calculate_turnout <- function(data, year) {
+  
+  # rename columns
+  data |>
+    rename(
+      fed_code = "Electoral District Number/Numéro de circonscription",
+      fed_name = "Electoral District Name/Nom de circonscription",
+      votes = "Total Votes/Total des votes",
+      electors = "Electors/Électeurs") |>
+    
+    # convert vote and elector counts as numeric type
+    mutate(votes = as.numeric(votes), electors= as.numeric(electors)) |>
+    
+    # get turnout rate for each electoral district
+    group_by(fed_code) |>
+    summarise(
+      total_votes = sum(votes, na.rm = TRUE),   # calculate sum of votes
+      electors = sum(electors, na.rm = TRUE)) |> # calculate sum of electors
+    ungroup() |>
+    
+    # calculate turnout rate
+    mutate(turnout = total_votes / electors, election_year = year)
+}
+
+# get turnout rate for each election
+election_2025_fed <- calculate_turnout(election_2025, 2025)
+
+election_2021_fed <- calculate_turnout(election_2021, 2021)
+
+election_2019_fed <- calculate_turnout(election_2019, 2019)
+
+# combine all election data
+election_all <- bind_rows(
+  election_2025_fed,
+  election_2021_fed,
+  election_2019_fed
+)
+
+# transform id column for merging two datasets
+news_fed <- news_fed |>
   mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+    fed_code = substr(
+      FEDDGUID_CEFIDUGD,
+      nchar(FEDDGUID_CEFIDUGD) - 4,
+      nchar(FEDDGUID_CEFIDUGD)
+    )
+  )
+
+# ensure id have the same data type for merging
+news_fed <- news_fed |>
+  mutate(fed_code = as.character(fed_code))
+
+election_all <- election_all |>
+  mutate(fed_code = as.character(fed_code))
+
+news_election <- news_fed |>
+  left_join(election_all, by = "fed_code")
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(
+  st_drop_geometry(news_election),
+  here("data", "02-analysis_data", "news_election.csv")
+)
