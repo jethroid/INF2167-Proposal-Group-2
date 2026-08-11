2de2c3a1bee352e6618195207f3f689d87ec7974
Jet McCullough <jet.mccullough@mail.utoronto.ca>
2026-08-11 05:38:29 -0400

    created some tables to use in paper

--- scripts/04-exploratory_data_analysis.R
+++ scripts/04-exploratory_data_analysis.R
@@ -25,4 +25,51 @@ withdupes |>
   style_tt(i=6:9, j=2, background = "#FFE0E0") |>
   style_tt(i=2:5, j=2, background = "#FFC2C2")
 
-view(withdupes)
+# look at the most negative changes by riding alongside lowest turnout in 2021 election
+
+# make table of worst news changes as of 2021
+
+badnews <- newselec |>
+  filter(election_year == 2021) |>
+  select(fedename, cumulative_news) |>
+  arrange(cumulative_news) |>
+  slice_head(n=23)
+
+# save vector of top names
+newsnames <- pull(badnews, fedename)
+
+badnews |>
+  tt(caption = "Ridings With The Most Negative News Impact Score", colnames = FALSE) |>
+  style_tt(i = "caption", bold = TRUE) |>
+  style_tt(i=1:5, j=2, background = "#F5C7FF") |>
+  style_tt(i=13:23, j=2, background = "#FCEBFF") |>
+  style_tt(i=6:12, j=2, background = "#F9DBFF")|>
+  style_tt(i=12, j=1, background = "#D1DAFF") |>
+  style_tt(i=15, j=1, background = "#D1DAFF") |>
+  style_tt(i=20, j=1, background = "#D1DAFF")
+  
+# make table of lowest turnout relative to ntl. avg.
+
+lowturn <- newselec |>
+  filter(election_year == 2021) |>
+  select(fedename, turnout_diff) |>
+  arrange(turnout_diff) |>
+  mutate(turnout_diff = percent(turnout_diff, accuracy = 0.1)) |>
+  slice_head(n=23)
+
+# save vector of top names
+turnnames <- pull(lowturn, fedename)
+
+lowturn |>
+  tt(caption = "Ridings With The Lowest Turnout Relative to National Average", colnames = FALSE) |>
+  style_tt(i = "caption", bold = TRUE) |>
+  style_tt(i=1:5, j=2, background = "#FFDB9E") |>
+  style_tt(i=13:23, j=2, background = "#FFF6E5") |>
+  style_tt(i=6:12, j=2, background = "#FFEBC7") |>
+  style_tt(i=23, j=1, background = "#D1DAFF") |>
+  style_tt(i=15, j=1, background = "#D1DAFF") |>
+  style_tt(i=19, j=1, background = "#D1DAFF") 
+
+#double check ridings in common, retroactively coloured in above
+
+intersect(newsnames, turnnames)
